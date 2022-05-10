b35c3c174c7385b089a24db40e268a4de4537530
LigthAlexey <karalkin@mail.ru>
2022-05-10 12:38:07 +0300

    change with unittest

--- test_abs_project.py
+++ test_abs_project.py
@@ -1,10 +1,13 @@
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number"
+import unittest
 
-def test_abs2():
-    assert abs(-42) == -42, "Should be absolute value of a number"
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+
+    def test_abs2(self):
+        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+
 
 if __name__ == "__main__":
-    test_abs1()
-    test_abs2()
-    print("Everything passed")
+    unittest.main()
